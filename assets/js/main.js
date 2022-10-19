867df4a1cb030980ed6e45e3c11c816b5f11b2af
paoloevan <paoloev1900@gmail.com>
2022-10-19 17:17:23 +0200

    add function prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ Dopo 30 secondi i numeri scompaiono
 
 e l'utente deve inserire, uno alla volta,
 i numeri che ha visto precedentemente, tramite il prompt().
+
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti
 e quali dei numeri da indovinare sono stati individuati.
 */
@@ -16,14 +17,21 @@ const containerEl = document.querySelector('.container');
 const timerEl = document.querySelector('.timer');
 
 //dichiaro variabile
-let seconds = 5;
 
 
-//genero cinque numeri casuali
-fiveRandom(5);
 
-//genero timer di 30 secondi e cancello i numeri
-timer();
+
+//genero cinque numeri casuali e li inserisco in array
+const numbers = arrayRandom(5);
+console.log(numbers, 'pc numbers');
+
+//cancello i numeri dopo 30 secondi
+clearNumber();
+
+//genero 5 prompt dopo 30 secondi e inserisco in array
+const myNumbers = myNumbersPrompt();
+console.log(myNumbers, 'my numbers');
+
 
 
 
@@ -31,16 +39,20 @@ timer();
 
 
 //funzione numeri casuali
-function fiveRandom(howMany) {
+function arrayRandom(howMany) {
+    const numbers = [];
     for (let i = 0; i < howMany; i++) {
-        number = Math.floor(Math.random() * 100);
+        let number = Math.floor(Math.random() * 100);
         containerEl.insertAdjacentHTML('afterbegin', `<div class="number">${number}</div>`);
+        numbers.push(number)
     }
+    return numbers;
 }
 
 
 //funzione timer e cancellazione numeri
-function timer() {
+function clearNumber() {
+    let seconds = 5
     const intervalId = setInterval(function () {
         seconds--
         console.log(seconds);
@@ -52,4 +64,22 @@ function timer() {
             }
         }
     }, 1000)
+}
+
+//funzione per generare array con prompt
+function myNumbersPrompt() {
+    let seconds = 5
+    const myNumbers = [];
+    const intervalId = setInterval(function () {
+        seconds--
+        if (seconds == 0) {
+            clearInterval(intervalId);
+            for (let i = 0; i < 5; i++) {
+                let myNumber = prompt('inserisci numero');
+                myNumbers.push(myNumber);
+                console.log('my numbers', myNumbers);
+            }
+        }
+    }, 1000)
+    return myNumbers;
 }
